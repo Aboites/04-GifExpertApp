f2096b04e2d83f6691dd99831d0768231a7fdeac
Rodrigo <rodrigoa459@gmail.com>
2020-10-25 20:02:00 -0600

    Funcionalidad para ver gifs en la pagina

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 // Functional component
 const GifExpertApp = () => {
 
     // const categories = ['Coding', 'Yorkie', 'Futbol'];
-    const [categories, setCategories] = useState(['Coding', 'Yorkie', 'Futbol']);
+    const [categories, setCategories] = useState(['Coding']);
 
     // const handleAdd = (e) => setCategories( [...categories, 'Chivas'] );
     // const handleAdd = (e) => setCategories( cats => [...cats, 'Chivas'] );
@@ -13,15 +14,18 @@ const GifExpertApp = () => {
     return (
         <>
             <h2>GifExpertApp</h2>
-            <AddCategory />
+            <AddCategory setCategories={ setCategories } />
             <hr />
 
 
             <ol>
                 {
-                    categories.map( category => {
-                    return <li key={ category }>{ category }</li>
-                    })
+                    categories.map( category => (
+                        <GifGrid 
+                            key={ category }
+                            category={ category } 
+                        />
+                    ))
                 }
             </ol>
         </>
